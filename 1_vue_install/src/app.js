4fd363b7f0ff1eac3035c7b7ed921eff6da1a038
Augusto008 <aquila.real0@gmail.com>
2023-12-18 08:03:23 -0300

    Feat: create a vue form
    Create a form and using vue variables and databind

--- 1_vue_install/src/app.js
+++ 1_vue_install/src/app.js
@@ -1,10 +1,20 @@
 const Users = {
     data() {
         return {
+            input: {
+                name: ""
+            },
             user_1: {
                 name: "Ambrósio",
                 age: 28,
-            }
+            },
+        }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault();
+            console.log(this.input.name);
+            this.user_1.name = this.input.name;
         }
     }
 }
