051d2f3847b075bb8c580820203006177ec71162
Augusto008 <aquila.real0@gmail.com>
2023-12-26 16:58:56 -0300

    Feat(`App.vue`): Import `LyfeCycle.vue` component

--- 2_vue_project/src/App.vue
+++ 2_vue_project/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <FirstComponent />
+  <div>
+    <FirstComponent />
+    <LifeCycle />
+  </div>
 </template>
 
 <script>
 
   import FirstComponent from './components/FirstComponent.vue';
+  import LifeCycle from './components/LifeCycle.vue';
 
   export default {
       name: 'App',
       components: {
-        FirstComponent
+        FirstComponent,
+        LifeCycle
       }
   }
 </script>
