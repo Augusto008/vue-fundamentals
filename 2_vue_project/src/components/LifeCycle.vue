01cb04529586755b1bfb8f91bb099038e8f6bf94
Augusto008 <aquila.real0@gmail.com>
2023-12-26 16:50:01 -0300

    Create `LifeCycle.vue`
    Create `LifeCycle.vue` containing some life cycle hooks using `setTimeout()` to see the functions overwriting each other.

---
+++ 2_vue_project/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>My name is {{ name }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LyfeCycle',
+        data() {
+            return {
+                name: 'Still don\'t know' 
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.name = "José"
+            }, 5000)
+        },
+        mounted() {
+            setTimeout(() => {
+                this.name = "Pedro"
+            }, 10000);
+        }
+    }
+</script>
