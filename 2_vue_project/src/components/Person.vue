1c119b89ff7e1c604fd932c23a36624d319f2d74
Augusto008 <aquila.real0@gmail.com>
2023-12-28 07:36:16 -0300

    Add an Avatar image to `Person.vue`

--- 2_vue_project/src/components/Person.vue
+++ 2_vue_project/src/components/Person.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    <Picture />
     <h2>{{ Person }}</h2>
     <Info />
   </div>
 </template>
 <script>
   import Info from './Info.vue';
+  import Picture from './Picture.vue';
   export default {
     name: 'Person',
     components: {
-        Info
+        Info,
+        Picture
     },
     data() {
       return {
