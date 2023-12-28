09fdf0b335ff5a508a246fd4a31fcb08f2c79841
Augusto008 <aquila.real0@gmail.com>
2023-12-28 05:54:01 -0300

    Create `Person.vue` and `Info.vue`
    `Person.vue` import the `Info.vue` component.

---
+++ 2_vue_project/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>I'm not working at the moment:</p>
+    <p>I use these languages:</p>
+    <ul>
+      <li>C</li>
+      <li>Java</li>
+      <li>JavaScript</li>
+      <li>PHP</li>
+      <li>Python</li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Info'
+  }
+</script>
---
+++ 2_vue_project/src/components/Person.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ Person }}</h2>
+    <Info />
+  </div>
+</template>
+<script>
+  import Info from './Info.vue';
+  export default {
+    name: 'Person',
+    components: {
+        Info
+    },
+    data() {
+      return {
+        name: "Ambrózio"
+      }
+    }
+  }
+</script>
