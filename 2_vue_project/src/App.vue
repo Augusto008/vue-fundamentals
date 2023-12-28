1acb9a617168d7080df028d1ab47f326c414266f
Augusto008 <aquila.real0@gmail.com>
2023-12-28 06:02:53 -0300

    Create `App.vue`
    Import `Person` and `ActionButton`.
    `ActionButton` was declared with attributes and function calls.

--- 2_vue_project/src/App.vue
+++ 2_vue_project/src/App.vue
@@ -2,6 +2,10 @@
   <div>
     <FirstComponent />
     <LifeCycle />
+    <Person />
+    <ActionButton id="button-id" classes="button-classes" type="button" content="Button Test" event="click" jsAction="buttonFunction" />
+    <ActionButton id="button2-id" classes="button-classes" type="button" content="Button2 Test" event="mouseover" jsAction="buttonActive" />
+    <ActionButton id="button3-id" classes="button-classes" type="button" content="Button3 Test" event="focus" jsAction="buttonMouseover" />
   </div>
 </template>
 
@@ -9,12 +13,16 @@
 
   import FirstComponent from './components/FirstComponent.vue';
   import LifeCycle from './components/LifeCycle.vue';
+  import Person from './components/Person.vue';
+  import ActionButton from './components/forms/elements/ActionButton.vue';
 
   export default {
-      name: 'App',
-      components: {
-        FirstComponent,
-        LifeCycle
-      }
+    name: 'App',
+    components: {
+      ActionButton,
+      FirstComponent,
+      LifeCycle,
+      Person
+    }
   }
 </script>
