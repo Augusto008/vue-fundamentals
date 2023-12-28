df51aa9e6bbb70014e79f809babcc8921763a53f
Augusto008 <aquila.real0@gmail.com>
2023-12-28 05:57:20 -0300

    Create `ActionButton.vue`
    Create a button component with attributes and multiple functions

---
+++ 2_vue_project/src/components/forms/elements/ActionButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button :id="id" :type="type" :class="classes" ref="btn">{{ content }}</button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: {
+    classes: String,
+    content: String,
+    event: String,
+    id: String,
+    jsAction: String,
+    type: String,
+  },
+  mounted() {
+    this.$refs.btn.addEventListener(this.event, this[this.jsAction]);
+  },
+  beforeDestroy() {
+    this.$refs.btn.removeEventListener(this.event, this[this.jsAction]);
+  },
+  methods: {
+    buttonFunction() {
+      alert("Button Clicked");
+    },
+    buttonActive() {
+      alert("Activated");
+      this.$refs.btn.setAttribute('active', 'true')
+    },
+    buttonMouseover() {
+      this.$refs.btn.setAttribute('style', 'background-color: gray; color: white;');
+    }
+  }
+};
+</script>
