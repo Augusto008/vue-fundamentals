806a44ff4d1a8b32c451f69f40f8499358710178
Augusto008 <aquila.real0@gmail.com>
2023-12-29 12:17:30 -0300

    Create `HeaderLinks.vue` components
    Create a nav elementand a scoped style tag

---
+++ 2_vue_project/src/components/HeaderLinks.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <h1>Logo</h1>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">News</a></li>
+      <li><a href="#">Contacts</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    margin: 0 2vw 0 2vw;
+  }
+  ul, nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  li {
+    margin-right: 20px;
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+    transition: 0.25s;
+  }
+  a:hover {
+    font-weight: bold;
+    transition: 0.25s;
+  }
+  h1 {
+    color: red;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'HeaderLinks'
+  }
+</script>
