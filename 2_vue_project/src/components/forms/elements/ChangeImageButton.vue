d6d10196b76ffcbc2c4adec217105ca832a70a8b
Augusto008 <aquila.real0@gmail.com>
2023-12-30 19:48:30 -0300

    Create `ChangeImageButton.vue` component
    Create a change image button to change the avatar image in the father element using a ``

---
+++ 2_vue_project/src/components/forms/elements/ChangeImageButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button @click="$emit('changeImage')">Change Image</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ChangeImageButton",
+    emits: ["changeImage"]
+  }
+</script>
