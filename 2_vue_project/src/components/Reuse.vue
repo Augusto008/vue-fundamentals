e28079e4b15efefdb9532e52cc2eabffa42f457e
Augusto008 <aquila.real0@gmail.com>
2023-12-30 12:52:12 -0300

    Create `Reuse.vue` component
    Create a button to be reused

---
+++ 2_vue_project/src/components/Reuse.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="turn">Click</button>
+    <span v-show="activate">Activated!</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Reuse',
+    data() {
+      return {
+        activate: false
+      }
+    },
+    methods: {
+      turn() {
+        this.activate = !this.activate
+      }
+    }
+  }
+
+</script>
