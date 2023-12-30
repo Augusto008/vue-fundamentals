bd8082a56f517cfe5dd5e919221ef9ed8abfe36c
Augusto008 <aquila.real0@gmail.com>
2023-12-30 10:45:57 -0300

    Create `Form1.vue` component
    Create component containing the formulary template and a method to alert the formulary submited data

---
+++ 2_vue_project/src/components/forms/templates/Form1.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit="send($event)" :action="action" :method="method" >
+    <loginLayout />
+  </form>
+</template>
+
+<style scoped>
+  form {
+    width: fit-content;
+    margin: 40px 10px 40px 10px;
+    padding: 0 10px 10px 10px;
+    border: 2px solid black;
+    border-radius: 10px;
+  }
+</style>
+
+<script>
+  import ActionButton from '../elements/ActionButton.vue';
+  import LoginLayout from '../layouts/LoginLayout.vue';
+  export default {
+    name: 'Form1',
+    props: {
+      action: String,
+      method: String
+    },
+    components: {
+      LoginLayout,
+      ActionButton
+    },
+    methods: {
+      send(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+
+        alert("Logged in: " + name + " => " + email);
+      }
+    }
+  }
+</script>
