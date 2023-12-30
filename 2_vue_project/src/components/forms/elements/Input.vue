47a1c18fc36fb5df8f511a8c49fc288ceaa5c299
Augusto008 <aquila.real0@gmail.com>
2023-12-30 10:33:14 -0300

    Create `Input.vue` Component
    Create the component containing input with label and its atributes

---
+++ 2_vue_project/src/components/forms/elements/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label :for="name">{{name}}</label>
+    <input :id="name" :type="type" :name="name" :placeholder="placeholder" :required="require" ref="input">
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    padding: 10px;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'Input',
+    props: {
+      id: String,
+      type: String,
+      require: Boolean,
+      placeholder: String,
+      value: String,
+      name: String
+    }
+  }
+</script>
