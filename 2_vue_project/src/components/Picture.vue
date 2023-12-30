ae16681b9aac61312572197201831d69168d31e2
Augusto008 <aquila.real0@gmail.com>
2023-12-30 20:01:34 -0300

    Feat(`Picture.vue`): Import `ChangeImageButton.vue` component
    Import `ChangeImageButton.vue` component calling the `@change-image` provided by `` to access the `changeImage()` function

--- 2_vue_project/src/components/Picture.vue
+++ 2_vue_project/src/components/Picture.vue
@@ -1,14 +1,27 @@
 <template>
-  <img :src="avatar" :alt="description" width="120px">
+  <div>
+    <img :src="avatar" :alt="description" width="120px">
+    <ChangeImageButton @change-image="changeImage" />
+  </div>
 </template>
 
 <script>
+  import ChangeImageButton from './forms/elements/ChangeImageButton.vue';
+
   export default {
     name: 'Picture',
     data() {
-      return {
-        avatar: 'img/wolf.webp',
-        description: "User Avatar"
+        return {
+            avatar: 'img/wolf.webp',
+            description: "User Avatar"
+        };
+    },
+    components: { 
+      ChangeImageButton 
+    },
+    methods: {
+      changeImage() {
+        this.avatar = 'img/abutre.jpg'
       }
     }
   }
