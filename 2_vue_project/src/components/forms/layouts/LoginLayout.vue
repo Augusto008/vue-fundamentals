6ad2865140aac16e33bdf425dd22fd4e83d02f6b
Augusto008 <aquila.real0@gmail.com>
2023-12-30 10:39:48 -0300

    Create `LoginLayout.vue` component
    Create component containing the layout for a login formulary

---
+++ 2_vue_project/src/components/forms/layouts/LoginLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <Input id="name" type="text" name="name" placeholder="name" require="true"/>
+    <Input id="email" type="email" name="email" placeholder="email" require="true"/>
+    <ActionButton id="login" type="submit" content="Send"/>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+  button {
+    width: fit-content;
+    margin: 10px;
+  }
+</style>
+
+<script>
+  import ActionButton from '../elements/ActionButton.vue';
+  import Input from '../elements/Input.vue';
+
+  export default {
+    name: 'LoginLayout',
+    components: {
+      ActionButton,
+      Input
+    }
+  }
+</script>
